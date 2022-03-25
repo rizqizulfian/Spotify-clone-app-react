c5891a7290e19e358277ad44868b9d491381989e
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-03-26 01:12:56 +0700

    [Refactor][004] - formatting style header/index.js

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -1,71 +1,65 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import { Link, useHistory } from 'react-router-dom';
-
+import { GiHamburgerMenu } from 'react-icons/gi';
+import api from '../../services/api';
 import './styles.css';
 
-import { GiHamburgerMenu } from 'react-icons/gi';
+const Header = ({ sidebarState }) => {
+  const [user, setUser] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false);
 
-import api from '../../services/api';
+  const history = useHistory();
 
-function Header({ sidebarState }) {
-    const [user, setUser] = useState('');
+  const loadUser = async () => {
+    const response = await api.get('/me');
+    setUser(response.data.display_name);
+    localStorage.setItem('user', response.data.display_name);
+  }
 
-    const [showDropdown, setShowDropdown] = useState(false);
+  useEffect(() => {
+    loadUser();
+  }, []);
 
-    const history = useHistory();
+  const logout = () => {
+    localStorage.removeItem('user');
+    history.push('/login');
+  }
 
-    async function loadUser() {
-        const response = await api.get('/me');
+  const dropdownMenu = useCallback(
+    () => setShowDropdown((value) => !value),
+    []
+  );
 
-        setUser(response.data.display_name);
-
-        localStorage.setItem('user', response.data.display_name);
-    }
-
-    useEffect(() => {
-        loadUser();
-    }, []);
-
-    function logout() {
-        localStorage.removeItem('user');
-        history.push('/login');
-    }
-
-    const dropdownMenu = useCallback(
-        () => setShowDropdown((value) => !value),
-        []
-    );
-
-    return (
-        <div id="header">
-            <div className="sidebar-toggle" onClick={sidebarState}>
-                <GiHamburgerMenu />
-            </div>
-            <div
-                className="user-menu"
-                onClick={dropdownMenu}
-                onBlur={() => setShowDropdown(false)}
-            >
-                <div className="user">{user}</div>
-                <div
-                    className="user-content"
-                    style={
-                        showDropdown
-                            ? { display: 'block' }
-                            : { display: 'none' }
-                    }
-                >
-                    <ul>
-                        <li>
-                            <Link to="/profile">Profil</Link>
-                        </li>
-                        <li onClick={logout}>Logout</li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div id="header">
+      <div className="sidebar-toggle" onClick={sidebarState}>
+        <GiHamburgerMenu />
+      </div>
+      <div
+        className="user-menu"
+        onClick={dropdownMenu}
+        onBlur={() => setShowDropdown(false)}
+      >
+        <div className="user">{user}</div>
+        <div
+          className="user-content"
+          style={
+            showDropdown
+              ? { display: 'block' }
+              : { display: 'none' }
+          }
+        >
+          <ul>
+            <li>
+              <Link to="/profile">Profil</Link>
+            </li>
+            <li onClick={logout}>Logout</li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Header;
